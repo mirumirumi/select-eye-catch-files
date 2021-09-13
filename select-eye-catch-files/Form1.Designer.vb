4f7e6876f1f174c33b29da7d0b0969dd48c4a322
mirumi <info@milmemo.net>
2021-09-13 09:30:57 +0900

    initial commit

---
+++ select-eye-catch-files/Form1.Designer.vb
@@ -0,0 +1,52 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'フォームがコンポーネントの一覧をクリーンアップするために dispose をオーバーライドします。
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Windows フォーム デザイナーで必要です。
+    Private components As System.ComponentModel.IContainer
+
+    'メモ: 以下のプロシージャは Windows フォーム デザイナーで必要です。
+    'Windows フォーム デザイナーを使用して変更できます。  
+    'コード エディターを使って変更しないでください。
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.go = New System.Windows.Forms.Button()
+        Me.SuspendLayout()
+        '
+        'go
+        '
+        Me.go.Font = New System.Drawing.Font("Myanmar Text", 12.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.go.Location = New System.Drawing.Point(252, 143)
+        Me.go.Name = "go"
+        Me.go.Size = New System.Drawing.Size(274, 138)
+        Me.go.TabIndex = 0
+        Me.go.Text = "go"
+        Me.go.UseVisualStyleBackColor = True
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(14.0!, 27.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(800, 450)
+        Me.Controls.Add(Me.go)
+        Me.Name = "Form1"
+        Me.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen
+        Me.Text = "Select eye-catch tool"
+        Me.ResumeLayout(False)
+
+    End Sub
+
+    Friend WithEvents go As Button
+End Class
---
+++ select-eye-catch-files/Form1.vb
@@ -0,0 +1,36 @@
+﻿Imports System.IO
+Imports System.Text
+
+Public Class Form1
+    Private Sub go_Click(sender As Object, e As EventArgs) Handles go.Click
+        Dim line As String = ""
+        Dim al As New ArrayList
+
+        Using sr As New StreamReader("[paths that eye catch file name list]", Encoding.GetEncoding("UTF-8"))
+            line = sr.ReadLine()
+            Do Until line Is Nothing
+                al.Add(line)
+                line = sr.ReadLine()
+            Loop
+        End Using
+
+        For Each myfile As String In al
+            Dim files As String() = Directory.GetFileSystemEntries("[directory to search from]", myfile, SearchOption.AllDirectories)
+            If files.Length <> 0 Then
+                For Each f In files
+                    Try
+                        File.Copy(f, "[directory to copy to]" & myfile)
+                    Catch ex As System.IO.FileNotFoundException
+                        Console.WriteLine(myfile)
+                    Catch ex As System.IO.DirectoryNotFoundException
+                        Console.WriteLine(myfile)
+                    Catch ex As System.IO.IOException 'imifumei
+                        Console.WriteLine("same files!!" & myfile)
+                    End Try
+                Next
+            End If
+        Next
+
+        MessageBox.Show("complete")
+    End Sub
+End Class
